b5435a1a7b94739d8d8d708dc9c42225fa2e35cf
MrRobot1413 <koshlyak.311@gmail.com>
2020-08-25 14:56:12 +0300

    Created class DBOperations

---
+++ app/src/main/java/aleksey/git/notessqlite/DBHelper.java
@@ -0,0 +1,31 @@
+package aleksey.git.notessqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Notes";
+    public static final String TABLE_NAME = "userNotes";
+    public static final String ID = "id";
+    public static final String NOTE = "note";
+    public static final String TIME = "time";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + ID
+                   + " INTEGER PRIMARY KEY," + NOTE + " TEXT," + TIME + " TEXT" + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/aleksey/git/notessqlite/MainActivity.java
+++ app/src/main/java/aleksey/git/notessqlite/MainActivity.java
@@ -3,7 +3,11 @@ package aleksey.git.notessqlite;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -11,28 +15,38 @@ import android.widget.ImageButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton mShowDialog;
-    AlertDialog.Builder mBuilder;
-    View mView;
-    TextInputLayout note;
-    Button btnAdd;
-    Button btnCancel;
-    AlertDialog dialog;
+    private ImageButton mShowDialog;
+    private AlertDialog.Builder mBuilder;
+    private View mView;
+    private TextInputLayout note;
+    private Button btnAdd;
+    private Button btnCancel;
+    private AlertDialog dialog;
+    private SimpleDateFormat formatter;
+    private Date date;
+    DBOperations operations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mShowDialog = findViewById(R.id.btnShowDialog);
+        operations = new DBOperations();
+        mShowDialog = findViewById(R.id.btnAddNote);
         mBuilder = new AlertDialog.Builder(MainActivity.this);
         mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
         note = mView.findViewById(R.id.noteField);
         btnAdd = mView.findViewById(R.id.btnAdd);
         btnCancel = mView.findViewById(R.id.btnCancel);
         mBuilder.setView(mView);
+        formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+3"));
+        date = new Date();
         dialog = mBuilder.create();
         mShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view) {
                         if(note.getEditText().getText().toString().trim().equals("")){
                             Snackbar.make(view, "Field can't be empty", Snackbar.LENGTH_LONG).show();
+                        } else{
+                            operations.addNote(note, formatter, date);
                         }
                     }
                 });
@@ -50,10 +66,12 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         dialog.dismiss();
+                        operations.dataToConsole();
                     }
                 });
             }
         });
+
     }
 
 }
